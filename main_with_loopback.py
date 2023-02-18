10117fdee869298e2a39d4bd2efe3458e916064b
Abduljwad Higaig <77650507+abduljwadhigaig@users.noreply.github.com>
2023-02-18 16:49:22 +0100

    Update main_with_loopback.py

--- main_with_loopback.py
+++ main_with_loopback.py
@@ -2,34 +2,37 @@ import requests
 import threading
 from dev_info import *
 from urllib3.exceptions import InsecureRequestWarning
-
+import time
+start = time.perf_counter()
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 def configure_device(xml_payload,**dev_inf):
-    response = requests.patch(
-	url = dev_inf["host"] ,
-	auth = (dev_inf["username"],dev_inf["password"]),
-	headers = dev_inf["headers"],
-	data = xml_payload ,
-	verify = False)
-    if response.status_code == 204:
-        print(" configuration successful",dev_inf["name"])
-    else:
-        print("Error configuring : " + str(response.status_code) + " " + response.text)
+    with threading.Lock(): # lock used to avoid race condition 
+        response = requests.patch(
+	    url = dev_inf["host"] ,
+	    auth = (dev_inf["username"],dev_inf["password"]),
+	    headers = dev_inf["headers"],
+	    data = xml_payload ,
+	    verify = False)
+        if response.status_code == 204:
+            print(" configuration successful",dev_inf["name"])
+        else:
+            print("Error configuring : " + str(response.status_code) + " " + response.text)
 
 def loobackN (xml_payload,**dev_inf):
-    for i in range(1,254):
-        xml_payload1= xml_payload.format(name=i,ip_address= i )
-        response = requests.patch(
-	url = dev_inf["host"] ,
-	auth = (dev_inf["username"],dev_inf["password"]),
-	headers = dev_inf["headers"],
-	data = xml_payload1 ,
-	verify = False)
-        if response.status_code == 204:
-           print(" loopback",i," successful",dev_inf["name"])
-        else:
-           print("Error configuring : " + str(response.status_code) + " " + response.text)
+    with threading.Lock():
+        for i in range(1,254):
+            xml_payload1= xml_payload.format(name=i,ip_address= i )
+            response = requests.patch(
+	    url = dev_inf["host"] ,
+	    auth = (dev_inf["username"],dev_inf["password"]),
+	    headers = dev_inf["headers"],
+	    data = xml_payload1 ,
+	    verify = False)
+            if response.status_code == 204:
+             print(" loopback",i," successful",dev_inf["name"])
+            else:
+             print("Error configuring : " + str(response.status_code) + " " + response.text)
 
 
 threads = []
@@ -41,13 +44,17 @@ for i in dev :
     threads.append(threading.Thread(target=loobackN,args=[open("loopback.xml").read()],kwargs=eval(dic_name)))
 
 for t in threads:
+    print(t,"is started")
     t.start()
 
 # Wait for all the threads to finish
 for t in threads:
+    print(t,"is waiting to be finishied")
     t.join()
 
+finish = time.perf_counter()
 
+print(f'Finished in {round(finish-start, 2)} second(s)')
 
 # interface gigabitEthernet 2
 # shutdown
